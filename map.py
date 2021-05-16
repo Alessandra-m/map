8bbf8ff7336442aff7002e692707e06b219e7ab3
Alessandra-m <alessandra.myzina.s@gmail.com>
2021-05-16 11:11:23 +0300

    Геолокация

--- map.py
+++ map.py
@@ -1,6 +1,7 @@
 import folium
 from folium import plugins
 from folium.plugins import Search
+from numpy import True_
 import openpyxl
 import json
 import os
@@ -8,7 +9,7 @@ import webbrowser
 
 #Создание карты 
 m = folium.Map(location=[45.0448, 38.976], zoom_start = 12)
-
+folium.TileLayer('cartodbpositron').add_to(m)
 
 #Загрузка данных 
 wb = openpyxl.reader.excel.load_workbook(filename="data.xlsx")
@@ -26,10 +27,9 @@ for i in range(2,49):
     adds = sheet_zero['B'+str(i)].value
     tel = sheet_zero['D'+str(i)].value
     folium.Marker(location=[sheet_zero['E'+str(i)].value, sheet_zero['F'+str(i)].value],
-                  popup(max_width=200) = '<strong>'+name+'\nАдрес</strong>: '+adds+'\nТелефон:</strong>'+tel,
+                  popup = '<strong>'+name+'\nАдрес</strong>: '+adds+'\nТелефон:</strong>'+tel,
                   tooltip = tooltip,
                   icon = vetIcon).add_to(m)
-
 
 #Расстановка маркеров зоомагазинов
 zoo = os.path.join('Icon','zoo.png')
@@ -55,7 +55,6 @@ for i in range(2,8):
                   tooltip = tooltip,
                   icon = priutIcon,).add_to(m)
 
-
 #Формирование границы Краснодара
 border = os.path.join('Информация','border.json')            
 folium.GeoJson(border, name = 'Krasnodar', 
@@ -65,7 +64,6 @@ folium.GeoJson(border, name = 'Krasnodar',
                                         "color": "black",
                                         "weight": 1.5,
                                         "dashArray": "4, 4"}).add_to(m)
-
 
 #Поисковик
 with open('searcher.json', 'r', encoding = 'utf-8') as f:
@@ -80,6 +78,9 @@ plugins.Search(geojson_obj,position = 'topleft',
 
 #Контроль уровней
 folium.LayerControl().add_to(m)
+
+#Геолокация
+plugins.LocateControl(auto_start=True).add_to(m)
 
 m.save("m.html")
 webbrowser.open("m.html")
