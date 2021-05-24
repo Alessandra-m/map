8fa375be1aea09133c4f78f27ad5ff320cebcd67
Alessandra-m <alessandra.myzina.s@gmail.com>
2021-05-24 20:42:02 +0300

    Обновленный поисковик и иконки

--- map.py
+++ map.py
@@ -22,7 +22,7 @@ wb.active = 0
 sheet_zero = wb.active
 vet = os.path.join('Icon','vet.png')
 for i in range(2,49):
-    vetIcon = folium.features.CustomIcon(vet, icon_size=(25, 25))
+    vetIcon = folium.features.CustomIcon(vet, icon_size=(22, 22))
     name = sheet_zero['A'+str(i)].value
     adds = sheet_zero['B'+str(i)].value
     tel = sheet_zero['D'+str(i)].value
@@ -36,7 +36,7 @@ zoo = os.path.join('Icon','zoo.png')
 wb.active = 1
 sheet_one = wb.active
 for i in range(2,8):
-    zooIcon = folium.features.CustomIcon(zoo, icon_size=(30, 30))
+    zooIcon = folium.features.CustomIcon(zoo, icon_size=(24, 24))
     folium.Marker(location=[sheet_one['E'+str(i)].value, sheet_one['F'+str(i)].value],
     popup ='<strong>'+ sheet_one['A'+str(i)].value + '\nАдрес</strong>: ' + sheet_one['B'+str(i)].value + '\nТелефон:</strong>' + sheet_zero['D'+str(i)].value,
     icon = zooIcon).add_to(m)
@@ -46,7 +46,7 @@ priut = os.path.join('Icon','priut.png')
 wb.active = 2
 sheet_two = wb.active   
 for i in range(2,8):
-    priutIcon = folium.features.CustomIcon(priut, icon_size=(32, 32))
+    priutIcon = folium.features.CustomIcon(priut, icon_size=(26, 26))
     name = sheet_two['A'+str(i)].value
     adds = sheet_two['B'+str(i)].value
     tel = sheet_two['D'+str(i)].value
@@ -60,7 +60,7 @@ cafe = os.path.join('Icon','Cafe.png')
 wb.active = 3
 sheet_three = wb.active   
 for i in range(2,26):
-    cafeIcon = folium.features.CustomIcon(cafe, icon_size=(35, 35))
+    cafeIcon = folium.features.CustomIcon(cafe, icon_size=(24, 24))
     name = sheet_three['A'+str(i)].value
     adds = sheet_three['B'+str(i)].value
     tel = sheet_three['C'+str(i)].value
@@ -74,7 +74,7 @@ park = os.path.join('Icon','park.png')
 wb.active = 4
 sheet_four = wb.active   
 for i in range(2,6):
-    parkIcon = folium.features.CustomIcon(park, icon_size=(30, 30))
+    parkIcon = folium.features.CustomIcon(park, icon_size=(26, 26))
     name = sheet_four['A'+str(i)].value
     adds = sheet_four['B'+str(i)].value
     tel = sheet_four['C'+str(i)].value
@@ -82,7 +82,6 @@ for i in range(2,6):
                   popup = '<strong>'+name+'\nАдрес</strong>: '+adds+'\nТелефон:</strong>'+tel,
                   tooltip = tooltip,
                   icon = parkIcon,).add_to(m)
-    
 
 #Формирование границы Краснодара
 border = os.path.join('Информация','border.json')            
--- Информация/Информайшен для поисковика.py
+++ Информация/Информайшен для поисковика.py
@@ -2,5 +2,5 @@ import openpyxl
 AL = openpyxl.reader.excel.load_workbook(filename="all.xlsx", data_only=True)
 AL.active = 0
 sheet_zero = AL.active
-for i in range(2,60):
+for i in range(2,89):
    print( '{"type": "Feature","properties": {"name":', sheet_zero['H'+str(i)].value,'},"geometry": {"type": "Point","coordinates": [',sheet_zero['F'+str(i)].value,',',sheet_zero['E'+str(i)].value,'] }}, ','\n')
